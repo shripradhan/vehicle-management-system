c0e683bb9435f0c1966db95fabcfd4f2e0c70247
Shrikant Pradhan <shripradhan.94@gmail.com>
2020-06-24 13:48:55 +0530

    fleet_app - Attached admin template and create ApplicationController for launching admin page

---
+++ fleet-app/src/main/java/com/shritech/fleetapp/ApplicationController.java
@@ -0,0 +1,13 @@
+package com.shritech.fleetapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+	@GetMapping("/index")
+	public String lounchHome() {
+		return "index";
+	}
+}
