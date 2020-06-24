0aec5237a256a44b795209968d8442822d68807d
Shrikant Pradhan <shripradhan.94@gmail.com>
2020-06-24 22:25:51 +0530

    fleet_app - Create All Service Classes

---
+++ fleet-app/src/main/java/com/shritech/fleetapp/service/VehicleStatusService.java
@@ -0,0 +1,12 @@
+package com.shritech.fleetapp.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+* @author Shrikant Pradhan
+*/
+
+@Service
+public class VehicleStatusService {
+
+}
